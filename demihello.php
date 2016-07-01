edcd5fc621a89bad17d4c6a495bccbe528430b91
Kamisuke <waza.ekd@gmail.com>
2016-07-01 15:09:31 +0200

    'demisukecommit'

--- demihello.php
+++ demihello.php
@@ -2583,15 +2583,17 @@ function processMessage($message) {
 			apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "HTML", "text" => "<b>".$insult.".</b>"));
 		} else {
 			$willBeTest = rand(1,10);
-			if($willBeTest < 10) {
+			if($willBeTest < 8) {
 				$insult = failInsult();
 				apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
 				usleep(500000);
 				apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "HTML", "reply_to_message_id" => $message_id, "text" => "<b>".$insult.".</b>"));
 			}
 			else {
+				$gif = "BQADBAADow4AAksYZAfIQMmy5CaN7gI";
 				//apiRequest("sendDocument", array('chat_id' => $chat_id, 'parse_mode' => "HTML", "reply_to_message_id" => $message_id, "text" => "BQADBAADow4AAksYZAcZ8kFXOQJ3uAI"));
-				apiRequest("sendDocument", array('chat_id' => $chat_id, 'parse_mode' => "HTML", "reply_to_message_id" => $message_id, "text" => "BQADBAADow4AAksYZAfIQMmy5CaN7gI"));
+				//apiRequest("sendDocument", array('chat_id' => $chat_id, 'parse_mode' => "HTML", "reply_to_message_id" => $message_id, "text" => "BQADBAADow4AAksYZAfIQMmy5CaN7gI"));
+				apiRequest("sendDocument", array('chat_id' => $chat_id, 'document' => $gif));
 			}
 		}
 	} else if (strpos(strtolower($text), "demisuke") !== false) {

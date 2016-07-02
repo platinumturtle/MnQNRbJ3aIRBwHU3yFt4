067d6e9dc07544ce55d021cd5d4d67f2cd604ec5
Kamisuke <waza.ekd@gmail.com>
2016-07-02 20:42:56 +0200

    'demisukecommit'

--- AAFDn4aaABtKARhDrtOdQrSdy4bMR0n-4eo/index.php
+++ AAFDn4aaABtKARhDrtOdQrSdy4bMR0n-4eo/index.php
@@ -2130,16 +2130,65 @@ function processMessage($message) {
 		$totalActive = $row['total_active'];
 		mysql_free_result($result);
 		mysql_close($link);
-		$result = "*Me llamo Demisuke. Soy hijo del Dios Yodo, famoso por ser el único ente del universo capaz de bautizar a alguien eternamente para ser reconocidos como hijos de Yodo por nuestras manchas marrones en la cabeza.".PHP_EOL.
-					PHP_EOL."Como hijo de Yodo, me puedes ver bautizado en mi foto de perfil, esas manchas marrones en la cabeza son inconfundibles, pero me quedan de lujo.".PHP_EOL.
-					"Mi archienemigo, al igual que el de todos los que hemos sido bautizados por Yodo, es Caballo Loco, el único inmune al bautismo por su ya predeterminado tono marrón.".PHP_EOL.
-					PHP_EOL."Cuenta la leyenda que hay varios seres superiores a Yodo, y que fueron ellos quienes bautizaron al mismísimo Yodo. Pero también cuenta que estos seres supremos, pese a poder transformarse en Caballo Loco, adoran a Yodo, y como prueba de ello le realizaron el Sagrado Ritual.".PHP_EOL.
-					PHP_EOL."Actualmente cultivo lonchas de york en escabeche, y trato de expandir la harmonía del imperio de Yodo más allá de sus tierras.".PHP_EOL.
-					"Para ello, me paseo alrededor del planeta Telegram en busca de nuevas civilizaciones, expandiendo la palabra de Yodo allá por donde voy. ¡Ya he logrado visitar ".$total." territorios distintos!".PHP_EOL.
-					PHP_EOL."Una de las tareas que realizo allá por donde voy es la de ayudar a los demás para esparcir la buena voluntad de Yodo, y para ello pongo al alcance de los habitantes con quien me cruzo una serie de funciones que les pueden interesar.".PHP_EOL.
-					PHP_EOL."Además llevo en mi mochila bien guardados una serie de vídeos sobre la historia de Yodo contada por sus ancestros vivientes, ¡que pese a llevarlos guardados algún que otro habitante me los ha visto! Aunque ellos comentan algo sobre huevos de pascua en cuanto los encuentran, no entiendo qué querrán decir...".PHP_EOL.
-					PHP_EOL."Actualmente tengo viajes planeados a ".$totalActive." territorios distintos para seguir ayudando a quien lo necesita, aunque cada día reviso si necesitan mi ayuda, así si no hay nadie los tacho de mi lista y tengo más tiempo para los demás.".PHP_EOL.
-					"¡Tengo ganas de visitar más sitios!*";
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*Me llamo Demisuke. Soy hijo del Dios Yodo, famoso por ser el único ente del universo capaz de bautizar a alguien eternamente para ser reconocidos como hijos de Yodo por nuestras manchas marrones en la cabeza.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*Como hijo de Yodo, me puedes ver bautizado en mi foto de perfil.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*Esas manchas marrones en la cabeza son inconfundibles, me quedan de lujo.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Mi archienemigo, al igual que el de todos los que hemos sido bautizados por Yodo, es Caballo Loco, el único inmune al bautismo por su ya predeterminado tono marrón.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Cuenta la leyenda que hay varios seres superiores a Yodo, y que fueron ellos quienes bautizaron al mismísimo Yodo.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(3);
+		$result = "*Pero también cuenta que estos seres supremos, pese a poder transformarse en Caballo Loco, adoran a Yodo, y como prueba de ello le realizaron el Sagrado Ritual.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Actualmente cultivo lonchas de york en escabeche, y trato de expandir la harmonía del imperio de Yodo más allá de sus tierras.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Para ello, me paseo alrededor del planeta Telegram en busca de nuevas civilizaciones, expandiendo la palabra de Yodo allá por donde voy.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*¡Ya he logrado visitar ".$total." territorios distintos!*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(3);
+		$result = "*Una de las tareas que realizo allá por donde voy es la de ayudar a los demás para esparcir la buena voluntad de Yodo, y para ello pongo al alcance de los habitantes con quien me cruzo una serie de funciones que les pueden interesar.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Además llevo en mi mochila bien guardados una serie de vídeos sobre la historia de Yodo contada por sus ancestros vivientes.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*¡Pese a llevarlos guardados algún que otro habitante me los ha visto ya!*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(2);
+		$result = "*Aunque cuando los encuentran comentan algo sobre huevos de pascua o algo así, no sé qué querrán decir...*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(3);
+		$result = "*Actualmente tengo viajes planeados a ".$totalActive." territorios distintos para seguir ayudando a quien lo necesita, aunque cada día reviso si necesitan mi ayuda, así si no hay nadie los tacho de mi lista y tengo más tiempo para los demás.*";
+		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
+		apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => "typing"));
+		sleep(1);
+		$result = "*¡Tengo ganas de visitar más sitios!*";
 		apiRequest("sendMessage", array('chat_id' => $chat_id, 'parse_mode' => "Markdown", "text" => $result));
 	} else if (strpos(strtolower($text), "roto2") !== false) {
 		error_log($logname." triggered: Roto2.");

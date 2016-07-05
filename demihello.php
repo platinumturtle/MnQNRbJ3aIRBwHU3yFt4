0924e86d90c210a24bd0215579a296e2e2475470
Kamisuke <waza.ekd@gmail.com>
2016-07-05 22:19:01 +0200

    'demisukecommit'

--- demihello.php
+++ demihello.php
@@ -3409,7 +3409,7 @@ if (isset($update["message"])) {
 		apiRequestJson("answerInlineQuery", ["inline_query_id" => $queryId, "results" => [
 		["type" => "article", "id" => "0", "title" => "Pulsa para enviar en negrita", "message_text" => $boldText, 'parse_mode' => "HTML",],
 		["type" => "article", "id" => "1", "title" => "Pulsa para crear Spoiler", "message_text" => "este no se como lo hare...",],
-		["type" => "article", "id" => "2", "title" => "Pulsa para enviar bocabajo", "message_text" => $pedorreta, 'parse_mode' => "HTML",],
+		//["type" => "article", "id" => "2", "title" => "Pulsa para enviar bocabajo", "message_text" => $pedorreta, 'parse_mode' => "HTML",],
 		]]);
 	}
 }

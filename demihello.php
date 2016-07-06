f26616ff0607032bbe49025796203e80de7534b2
Kamisuke <waza.ekd@gmail.com>
2016-07-06 18:52:54 +0200

    'demisukecommit'

--- demihello.php
+++ demihello.php
@@ -3401,10 +3401,14 @@ if (isset($update["message"])) {
 		//$pedorreta = str_replace("g","n",$claveles);
 		$claveles = "<a href='http://telegram.me/DemisukeBot'>".$text."</a>";
 		
-		$boton = (object) [ "text" => "porfa", "url" => "http://google.es", ];
+		$object = new stdClass();
+		$object->text = 'Here we go';
+		$object->url = 'http://google.es';
+		
+		$boton = [ "text" => "porfa", "url" => "http://google.es", ];
 		
 		apiRequestJson("answerInlineQuery", ["inline_query_id" => $queryId, "results" => [
-		["type" => "article", "id" => "0", "title" => "Pulsa para crear Spoiler", "message_text" => "este no se como lo hare...", "reply_markup" => ["inline_keyboard" => [[ [[ $boton ]], ]] ], ],
+		["type" => "article", "id" => "0", "title" => "Pulsa para crear Spoiler", "message_text" => "este no se como lo hare...", "reply_markup" => ["inline_keyboard" => [[ [[ $object ]], ]] ], ],
 		["type" => "article", "id" => "1", "title" => "Pulsa para enviar en negrita", "message_text" => $boldText, 'parse_mode' => "HTML",],
 		["type" => "article", "id" => "2", "title" => "Pulsa para enviar en azul", "message_text" => $claveles, 'parse_mode' => "HTML", 'disable_web_page_preview' => TRUE],
 		]]);

0250f21afe919b60248398279442fb66c79ca21f
Kamisuke <waza.ekd@gmail.com>
2016-07-07 01:21:25 +0200

    'demisukecommit'

--- demihello.php
+++ demihello.php
@@ -3557,12 +3557,12 @@ class spoiler {
     private static $Key = "dublin";
  
     public static function encrypt ($input) {
-        $output = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), $input, MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))));
+        $output = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5(spoiler::$Key), $input, MCRYPT_MODE_CBC, md5(md5(spoiler::$Key))));
         return $output;
     }
  
     public static function decrypt ($input) {
-        $output = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), base64_decode($input), MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))), "\0");
+        $output = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5(spoiler::$Key), base64_decode($input), MCRYPT_MODE_CBC, md5(md5(spoiler::$Key))), "\0");
         return $output;
     }
  

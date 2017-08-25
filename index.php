71b99f1e7a55b5ba5ad141f29b8a00b7e0dd9dd2
platinumturtle <twitifruity@gmail.com>
2017-08-26 01:57:25 +0200

    demisukecommitPHPMA

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ print_r("<head>");
 print_r("<title>?</title>");
 print_r("<link rel='icon' href='/favicon.png' type='image/png' />");
 print_r("</head>");
-print_r("<body style='background: pink;'>");
+print_r("<body style='background: red;'>");
 print_r("<center>");
 print_r("<img src='/logo.png' />");
 print_r("</center>");

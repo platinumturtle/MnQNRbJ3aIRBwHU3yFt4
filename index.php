ea626d2c099776f5a5fd263b88ed64c09b543480
platinumturtle <twitifruity@gmail.com>
2017-08-26 03:22:11 +0200

    'demisukecommit'

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 function dbConnect() {
 	$user = "drupal_luci";
 	$pass = "vRsz68!0";
-	$server = "ns3028663.ip-5-135-189.eu";
+	$server = "5.135.189.62:8443";
 	$db = "laluciernaga_drupal";
 	$con = mysqli_connect($server,$user,$pass) or die('No se pudo conectar: ' . mysqli_error());
 	mysqli_select_db($con, $db) or die('No se pudo seleccionar la base de datos');
